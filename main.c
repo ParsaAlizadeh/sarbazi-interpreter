e83ff9368f77ababa21b44446f874d6a8384c181
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-10-27 10:22:14 +0330

    Implement hw4 machine

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main() {
     PrintProgram(p, stdout);
     CodeStream *cs = NewCode(stdout);
     for (Instruct *ins = p->ins; ins != NULL; ins = ins->next) {
-        int opcode;
+        /* hw1 machine
         opcode = InstructMatchN(ins,
                                 "mov R1, R2",
                                 "add R1, R2",
@@ -36,7 +36,7 @@ int main() {
                                 "jne R1, R2, @a",
                                 "jl R1, R2, @a",
                                 "jh R1, R2, @a",
-                                "call R1, @a, R2",
+                                "call R1, (a), R2",
                                 (char *)NULL);
         if (opcode != -1) {
             CodeWrite(cs, "4D6D6D16%",
@@ -63,9 +63,49 @@ int main() {
                       InstructOpData(ins, 0)->num);
             CodeFlush(cs);
             continue;
+        } */
+        /* hw4 machine */
+        int opcode = -1;
+        opcode = InstructMatchN(ins,
+                                "load #1",
+                                "load (a)",
+                                "store #1",
+                                "store (a)",
+                                "add #1",
+                                "sub #1",
+                                "addi #1",
+                                "subi #1",
+                                "jmpl @a",
+                                (char *)NULL);
+        if (opcode != -1) {
+            CodeWrite(cs, "8D", opcode);
+            Token *tok = InstructOp(ins, 0);
+            if (tok->type == TOK_OP_ADDR)
+                CodeWrite(cs, "32%", "?");
+            else
+                CodeWrite(cs, "32D", tok->num);
+            CodeFlush(cs);
+            continue;
+        }
+        opcode = InstructMatchN(ins,
+                                "jnz #1",
+                                "jz #1",
+                                "jneg #1",
+                                "jpos #1",
+                                "jmp #1");
+        if (opcode != -1) {
+            CodeWrite(cs, "%7D8D", "1", opcode, InstructOp(ins, 0)->num);
+            CodeFlush(cs);
+            continue;
+        }
+        if (InstructMatch(ins, "dw #1")) {
+            CodeWrite(cs, "32D", InstructOp(ins, 0)->num);
+            CodeFlush(cs);
+            continue;
         }
     }
     FreeCode(cs);
     FreeProgram(p);
+    FreeToken(tok);
     return EXIT_SUCCESS;
 }
